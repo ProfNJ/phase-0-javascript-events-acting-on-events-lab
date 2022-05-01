9020260c92e96a362be248b7bef365eb521e56a2
Rory Grant <rorygran5@gmail.com>
2022-05-01 19:15:57 -0400

    commit tree c4a58a89ed35c3c82a57b261b59a63d142a974af

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+    if(event.key === "ArrowLeft"){
+        moveDodgerLeft();
+    }
+    if(event.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+  });
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#6b0";
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
